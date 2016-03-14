5ae5dad594c9de5398dd74eb1c61386ef6416829
Théo Guérin <theo.guerin.pro@gmail.com>
2016-03-14 20:51:03 +0100

    Création de 4 joueurs dans le client de test et corréction de la fonction d'affichage

--- Server/TestClient.py
+++ Server/TestClient.py
@@ -1,7 +1,9 @@
 #! /bin/ipython3
 
 import json
+import random
 import time
+import math
 import requests
 import threading
 import pygame
@@ -19,44 +21,49 @@ screen.fill(WHITE)
 pygame.display.flip()
 
 def affGame(game,screen):
+    nbSpheres = 0
     screen.fill([255, 255, 255])
     for joueur in game.values():
         color= (random.randint (0,255),random.randint (0,255),
                             random.randint (0,255))
         for sphere in joueur:
+            nbSpheres +=1
             x=sphere[0][0]
             y=sphere[0][1]
             s=sphere[1]
             pygame.draw.circle(screen, color, [int(x/20),int(y/20)], int(math.sqrt(s)/2))
     pygame.display.flip()
-
+    return nbSpheres
 
 def client(ip, port, screen):
     tick = 0
     erreur = 0
 
-    jsonHeaders = {'content-type': 'application/json'}
-
-    #player=input("Username ?:")
-    player = "Nyda"
-    data = dict()
-    data["Username"]=player
-    data["Ia"]="rand"
+    testJoueurs =[
+        "/?username=P1&ia=RandIa",
+        "/?username=P2&ia=RandIa",
+        "/?username=P3&ia=RandIa",
+        "/?username=P4&ia=RandIa",
+    ]
 
     url = 'http://'+str(ip)+':'+str(port)
 
-    rep = requests.post(url, data=json.dumps(data,default=lambda o: o.__dict__), headers=jsonHeaders)
+    for joueur in testJoueurs:
+        requests.get(url+joueur)
 
     while True:
         tick +=1
+        nb = 0
         try:
-            r = requests.get(url)
-            print(r.text)
-            game = json.loads(r.text)
-            affGame(game,screen)
+            r = requests.get(url).text
+            game = json.loads(r)
+            nb = affGame(game,screen)
         except:
+            print("error "+str(erreur))
             erreur += 1
-        time.sleep(1000/30)
+        if tick % 120 ==0:
+            print("Nombre de spheres: "+str(nb))
+        time.sleep(1/60)
 
 
 client(ip,port,screen)
