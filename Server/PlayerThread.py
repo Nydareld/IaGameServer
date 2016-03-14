09804500db44de203f5bc194c04ba04b09909aeb
Quentin Picard <crashleur@gmail.com>
2016-03-14 18:00:26 +0100

    Implémentation de la mise à jour du calcul de position et Implémentation de join

--- Server/PlayerThread.py
+++ Server/PlayerThread.py
@@ -1,5 +1,7 @@
 from Game import *
 from threading import Thread
+import threading
+import time
 
 
 class PlayerThread(Thread):
@@ -8,33 +10,54 @@ class PlayerThread(Thread):
         Thread.__init__(self)
         self.GameThread = GameThread
         self.username = username
-        GameThread.barrierEtape._parties = GameThread.barrierTours._parties
+        GameThread.barrierTours._parties += 1
         self.ia = ia
+        self.joueur = Player(ia,username,GameThread.game.gamesize)
         #GameThread.game.joueurs[username]=Player(ia,username,GameThread.game.gamesize)
-        GameThread.game.addJoueur(Player(ia,username,GameThread.game.gamesize))
+        GameThread.game.addJoueur(self.joueur)
+
+        GameThread.nbth += 1
 
 
     def run(self):
         while True:
             #attend le début du tours
+            print("Barriere debut de tours "+str(threading.current_thread().name))
+            print(self.GameThread.barrierTours.parties)
             self.GameThread.barrierTours.wait()
-
             #execute le code de l'IA
             self.executeIa()
 
+
+            print(self.GameThread.barrierEtape.parties)
             self.GameThread.barrierEtape.wait()
 
             self.calculePos()
 
             self.GameThread.barrierEtape.wait()
 
-            self.join()
+            aManger = self.join()
+
+            self.GameThread.barrierManger.wait()
+            time.sleep(1)
+
 
     def executeIa(self):
         pass
 
     def calculePos(self):
+        for sphere in  self.joueur.spheres:
+            sphere.vectPos = sphere.posNextTick()
         pass
 
     def join(self):
-        pass
+        listjoueur = dict()
+        for sphere in  self.joueur.spheres:
+            for joueur2 in self.GameThread.game.joueurs.values():
+                for sphere2 in joueur2.spheres:
+                    res = sphere.join(sphere2,joueur2)
+                    if(res != None):
+                        if(not (listjoueur[res[0].username] in locals)):
+                            listjoueur[res[0].username] = []
+                        listjoueur[res[0].username].append(res[1])
+        return listjoueur
