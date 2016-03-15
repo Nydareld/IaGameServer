3675772e30ee867f7cc59d0c58dece5ff461faf1
Théo Guérin <theo.guerin.pro@gmail.com>
2016-03-15 23:06:39 +0100

    Récupétation des data du jeu avec l'url /data et non un fichier

--- CodIa/tuto/static/game.js
+++ CodIa/tuto/static/game.js
@@ -20,29 +20,33 @@ window.addEventListener('resize', resize, false);
 function init() {
 
     resize();
-    
- 
 
-  
+
+
+
 
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
-    $.getJSON("/static/sample.json", function(json) {
-      
-    $.each(json, function(key, val){
-     
-      for (j=0; j<val.length; j++){
-        
-         var circle = new createjs.Shape();
-         circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, (val[j][1])*3);
-         circle.x = val[j][0][0]/10;
-         circle.y = val[j][0][1]/10;
-         
-         
-         stage.addChild(circle);}
+
+
+    $.ajax({
+        type: "GET",
+        url: "/data",
+        dataType: "json",
+        success: function(json) {
+            $.each(json, function(key, val){
+                for (j=0; j<val.length; j++){
+
+                    var circle = new createjs.Shape();
+                    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, (val[j][1])*3);
+                    circle.x = val[j][0][0]/10;
+                    circle.y = val[j][0][1]/10;
+                    stage.addChild(circle);
+                }
+            })
+        }
     });
-     
+
    stage.update();
     // this will show the info it in firebug console
-});
 }
